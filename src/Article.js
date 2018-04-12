049a500f67499a2e6443bc817b048096f2b7f554
cseal <19mann84@gmail.com>
2018-04-12 17:58:43 +0300

    Lesson 1 with components on class structure. State of show comments insert in CommentList component(not in Article component)

--- src/Article.js
+++ src/Article.js
@@ -1,22 +1,12 @@
 "use strict";
 import React, {Component} from  "react";
 import CommentList from "./CommentList";
-/*
-export default function Articles(props){
-    let {title, text} = props;
-    return (
-        <div>
-            <h3>{title}</h3>
-            <section>{text}</section>
-        </div>
-    )
-}*/
+
 export default class Article extends Component{
     constructor(props){
         super(props);
         this.state = {
             isOpen: false,
-            commentsOpen: false
         }
     }
 
@@ -24,33 +14,17 @@ export default class Article extends Component{
         this.setState({
             isOpen: !this.state.isOpen,
         });
-        if(this.state.isOpen){
-            this.setState({
-                commentsOpen: false
-            })
-        }
     };
-    toggleComments(){
-        this.setState({
-            commentsOpen: !this.state.commentsOpen
-        })
-    }
-
-    showCommentsBtn(length = 0) {
-        const {commentsOpen} = this.state;
-        if(length !== 0){
-            return <button onClick={()=>this.toggleComments()}>{(commentsOpen)?'close Comments':'show Comments'}</button>
-        }
-        return null;
-    }
 
     getBody(){
      let {isOpen, commentsOpen} = this.state;
-     let {date, text, comments = []} = this.props.article;
+     let { text, comments = []} = this.props.article;
         if(!isOpen) return null;
-        return (<React.Fragment><section>{text}</section>
-            {(commentsOpen)?<CommentList comments={comments} id={date}/> : null}
-            {this.showCommentsBtn(comments.length)}<br /><br /></React.Fragment>)
+        return <React.Fragment>
+                 <section>{text}</section>
+                 {(comments.length !== 0)?<CommentList comments={comments}/> : null}
+                 <br /><br />
+               </React.Fragment>
     };
 
     render() {
--- src/CommentList.js
+++ src/CommentList.js
@@ -1,23 +1,40 @@
 'use strict';
-//import React,{Component} from "react";
-import React from "react";
+import React,{Component} from "react";
 import Comment from "./Comment";
 
-/*export default class CommentList extends Component{
+export default class CommentList extends Component{
     constructor(props){
         super(props);
         this.state = {
             isOpen: false
         }
     }
-    render(){
-        const {comment} = this.props;
-    }
-}*/
 
-export default function CommentList({comments, id}){
-    let commentElements = comments.map(commentData=><li key={commentData.id}><Comment comment={commentData}/></li>);
-    return (<ul>
-        {commentElements}
-            </ul>);
+    toggleComments(){
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    };
+
+    getBody(){
+        const {comments = []} = this.props;
+        const {isOpen} = this.state;
+        const commentsElement = comments.map(commentData=><li key={commentData.id}><Comment comment={commentData}/></li>);
+        if(!isOpen || commentsElement.length === 0) return null;
+        return (<ul>
+                {commentsElement}
+                </ul>)
+    }
+    getButton(){
+        const {isOpen} = this.state;
+        return <button onClick={()=>this.toggleComments()}>{(isOpen)?'close Comments':'show Comments'}</button>
+    }
+
+    render(){
+        const {isOpen} = this.state;
+        return (<React.Fragment>
+                    {this.getBody()}
+                    {this.getButton()}
+               </React.Fragment>)
+    }
 }
