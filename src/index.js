097792205b9c463660bc3d90f463781fccc8826b
cseal <19mann84@gmail.com>
2018-06-01 17:14:29 +0300

    Task 4 complit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import {render} from "react-dom";
-import {articles as articleData} from "./fixtures";
-import ArticleList from "./components/ArticleList";
-render(<ArticleList articleData={articleData}/>, document.getElementById('container'));
+import {Provider} from 'react-redux';
+
+import {store} from './store';
+import App from "./App";
+render(<Provider store={store}>
+            <App/>
+        </Provider>, document.getElementById('container'));
+
